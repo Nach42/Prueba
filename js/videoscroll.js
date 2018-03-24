b9539f07c732cf750263303b5451d6c98ec4aa1a
Ignacio Dones <ignacio.dones@alu.uclm.es>
2018-03-24 16:50:35 +0100

    plugin video

--- js/videoscroll.js
+++ js/videoscroll.js
@@ -1,10 +1,14 @@
 (function($){
 	$.fn.vscroll = function(){
 		var video = $(this);
+		var height = video.height();
+		var width = video.width();
 		$(window).scroll(function(){
 			var posicion = $(window).scrollTop();
 			if(posicion >= 380){
 				video.css({position:"fixed", width:"280px", height:"160px", bottom:"10px", left:"10px"});
+			}else if(posicion < 300){
+				video.css({position:"relative",width:width, height:height});
 			}
 		});
 	};
